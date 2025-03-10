66edaf39f5d6f1dff09dda639adf4e86251894f6
Onelots <onelots@onelots.fr>
2025-03-10 12:31:48 +0100

    laurel_sprout: Welcome to EvolutionX
    Signed-off-by: Onelots <onelots@onelots.fr>

--- device.mk
+++ device.mk
@@ -61,7 +61,8 @@ $(call soong_config_set,surfaceflinger,udfps_lib,//hardware/xiaomi:libudfps_exte
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(DEVICE_PATH)/overlay \
-    $(DEVICE_PATH)/overlay-lineage
+    $(DEVICE_PATH)/overlay-lineage \
+    $(DEVICE_PATH)/overlay-evolution
 
 PRODUCT_PACKAGES += \
     ApertureLaurel
